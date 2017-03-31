1242610a36fafd486f8b9a1c811ec4801b45b934
Hannah Schuster <hschuster9@gmail.com>
2017-03-31 17:11:37 -0400

    delete function and redirecting working

--- public/js/app.js
+++ public/js/app.js
@@ -77,14 +77,13 @@ function ItemNewControllerFunction(ItemFactory, $state){
 
 function ItemShowControllerFunction( ItemFactory, $state, $stateParams){
   this.item = ItemFactory.get({title: $stateParams.title})
+  this.destroy = function(){
+    this.item.$delete({ title: $stateParams.title}).then(function(){
+      $state.go("index")
+    })
+  }
   this.update = function(){
-      this.item.$update({ title: $stateParams.title}).then(function(){
-        $state.go("show")
-      })
+      this.item.$update({ title: $stateParams.title})
     }
-    this.destroy = function(){
-      this.item.$delete({ title: $stateParams.title}).then(function(){
-        $state.go("index")
-      })
-    }
+
   }
